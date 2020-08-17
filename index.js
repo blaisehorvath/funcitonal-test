e462adc92a699b92649d8ac535fd65e0fbadedf2
horvath-balazs <balazs.horvath@jaystack.com>
2020-08-17 18:33:04 +0200

    Verbosity

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const people = require("./data")
 const { task, task2 } = require("./lib")
 
 const result1 = task(people)
-console.log(JSON.stringify(result1, null, 2))
+console.log("result of task 1:",JSON.stringify(result1, null, 2))
 
 const result2 = task2(people)
-console.log(JSON.stringify(result2, null, 2))
-
+console.log("result of task 1:", JSON.stringify(result2, null, 2))
